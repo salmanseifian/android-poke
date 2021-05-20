775cf992dae1ea45d7d1546ba73a8cc7ea880baf
Salman <salman.seifian@gmail.com>
2021-05-21 01:36:07 +0430

    [+] PokemonSpeciesResponse

---
+++ app/src/main/java/com/salmanseifian/androidpoke/model/PokemonSpeciesResponse.kt
@@ -0,0 +1,21 @@
+package com.salmanseifian.androidpoke.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonSpeciesResponse(
+    @SerializedName("count")
+    val count: Int?,
+    @SerializedName("next")
+    val next: String?,
+    @SerializedName("previous")
+    val previous: Any?,
+    @SerializedName("results")
+    val pokemonSpecies: List<PokemonSpecies>
+)
+
+data class PokemonSpecies(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("url")
+    val url: String
+)
