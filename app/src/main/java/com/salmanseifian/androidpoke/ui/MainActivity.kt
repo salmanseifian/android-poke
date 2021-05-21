e4cf91059beddec601e2068b42998f6695b9bd04
Salman <salman.seifian@gmail.com>
2021-05-21 10:31:59 +0430

    [+] Set UI as aEntryPoint

--- app/src/main/java/com/salmanseifian/androidpoke/ui/MainActivity.kt
+++ app/src/main/java/com/salmanseifian/androidpoke/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.salmanseifian.androidpoke
+package com.salmanseifian.androidpoke.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.salmanseifian.androidpoke.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/salmanseifian/androidpoke/ui/PokemonSpeciesFragment.kt
@@ -0,0 +1,8 @@
+package com.salmanseifian.androidpoke.ui
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PokemonSpeciesFragment : Fragment() {
+}
