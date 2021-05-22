d07cd4ffed4efe6037753a6b3134b2d08ed48575
Salman <salman.seifian@gmail.com>
2021-05-23 01:42:42 +0430

    Add Pokemon Details Response Model

---
+++ app/src/main/java/com/salmanseifian/androidpoke/data/model/PokemonDetailsResponse.kt
@@ -0,0 +1,43 @@
+package com.salmanseifian.androidpoke.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonDetailsResponse(
+    @SerializedName("evolution_chain")
+    val evolutionChain: EvolutionChain?,
+    @SerializedName("flavor_text_entries")
+    val flavorTextEntries: List<FlavorTextEntry>?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("name")
+    val name: String?,
+)
+
+data class EvolutionChain(
+    @SerializedName("url")
+    val url: String?
+)
+
+data class FlavorTextEntry(
+    @SerializedName("flavor_text")
+    val flavorText: String?,
+    @SerializedName("language")
+    val language: Language?,
+    @SerializedName("version")
+    val version: Version?
+)
+
+data class Language(
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("url")
+    val url: String?
+)
+
+data class Version(
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("url")
+    val url: String?
+)
