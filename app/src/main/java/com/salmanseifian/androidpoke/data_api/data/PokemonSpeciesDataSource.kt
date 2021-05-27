23eef8fbead87f08699baf1807b8030b6047cb66
Salman <salman.seifian@gmail.com>
2021-05-27 22:06:38 +0430

    Covert API Response to Repository Model in data source

--- app/src/main/java/com/salmanseifian/androidpoke/data_api/data/PokemonSpeciesDataSource.kt
+++ app/src/main/java/com/salmanseifian/androidpoke/data_api/data/PokemonSpeciesDataSource.kt
@@ -2,26 +2,31 @@ package com.salmanseifian.androidpoke.data_api.data
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.salmanseifian.androidpoke.data.model.PokemonRepositoryModel
 import com.salmanseifian.androidpoke.data_api.PokeService
-import com.salmanseifian.androidpoke.data_api.model.PokemonSpecies
+import com.salmanseifian.androidpoke.data_api.mapper.PokemonSpeciesResponseToRepositoryModelMapper
 import com.salmanseifian.androidpoke.utils.STARTING_OFFSET_INDEX
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
-class PokemonSpeciesDataSource @Inject constructor(private val pokeService: PokeService) :
-    PagingSource<Int, PokemonSpecies>() {
+class PokemonSpeciesDataSource @Inject constructor(
+    private val pokeService: PokeService,
+    private val pokemonSpeciesResponseToRepositoryModelMapper: PokemonSpeciesResponseToRepositoryModelMapper
+) :
+    PagingSource<Int, PokemonRepositoryModel>() {
 
-    override fun getRefreshKey(state: PagingState<Int, PokemonSpecies>): Int? =
+    override fun getRefreshKey(state: PagingState<Int, PokemonRepositoryModel>): Int? =
         state.anchorPosition
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PokemonSpecies> {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PokemonRepositoryModel> {
         val offset = params.key ?: STARTING_OFFSET_INDEX
         val loadSize = params.loadSize
         return try {
             val response =
                 pokeService.getPokemonSpecies(offset, params.loadSize)
-            val allSpecies = response.pokemonSpecies
+            val allSpecies =
+                pokemonSpeciesResponseToRepositoryModelMapper.toRepositoryModel(response).pokemons
             LoadResult.Page(
                 data = allSpecies,
                 prevKey = if (offset == STARTING_OFFSET_INDEX) null else offset - loadSize,
