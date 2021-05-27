f9bc15ef44bbbae538054c5c94f667028deef4b7
Salman <salman.seifian@gmail.com>
2021-05-27 21:33:41 +0430

    Create PokemonRepositoryModel

---
+++ app/src/main/java/com/salmanseifian/androidpoke/data/model/PokemonRepositoryModel.kt
@@ -0,0 +1,8 @@
+package com.salmanseifian.androidpoke.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonRepositoryModel(
+    val name: String,
+    val url: String
+)
