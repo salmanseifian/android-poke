7e7778d766ef899a97bcc7cf25e10d790b279869
Salman <salman.seifian@gmail.com>
2021-05-28 19:08:34 +0430

    Create Test File for PokemonSpeciesViewModel

---
+++ app/src/test/java/com/salmanseifian/androidpoke/presentation/PokemonSpeciesViewModelTest.kt
@@ -0,0 +1,19 @@
+package com.salmanseifian.androidpoke.presentation
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+
+
+class PokemonSpeciesViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
